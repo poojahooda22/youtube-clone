31af828d5ae4e2855e8e18c5dba60646d01f9b66
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-05 22:01:53 +0530

    updated

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -9,6 +9,7 @@ const videos = ({videos}) => {
       direction="row" flexWrap="wrap" justifyContent="start" gap={2}
     >{videos.map((item, idx) =>  (
       <Box key={idx}>
+        {item.id.videoId && <VideoCard video={item} />}
 
       </Box>
     ))}
