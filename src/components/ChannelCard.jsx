3fa1cf01de84dfdd0e2c5e52c220be8d4f05d0ba
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-06 23:05:03 +0530

    channel updated

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { Box, CardContent, CardMedia, Typography} from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { Link} from 'react-router-dom';
+import { demoProfilePicture } from '../utils/constants';
 
-function ChannelCard() {
-  return (
-    <div>
-      ChannelCard
-    </div>
-  )
-}
+
+
+const ChannelCard = ({channelDetail}) => (
+
+    <Box
+      sx={{
+        boxShadow: 'none',
+        borderRadius: '20px',
+      }}
+    >
+      <Link
+        to={`/channel/${channelDetail?.id?.channelId}`}
+      >
+        <CardContent sx={{ display:  'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', colo: '#fff'}}>
+          <CardMedia 
+            image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+            alt={channelDetail?.snippet?.title}
+            sx={{ borderRadius: '50%', height: '180px',
+          width: '180px', mb: 2, border: '1px solid #e3e3e3'}}
+          />
+          <Typography variant='h6'>
+            {channelDetail?.snippet?.title}
+          </Typography>
+        </CardContent>
+
+      </Link>
+    </Box>
+)
 
 export default ChannelCard
