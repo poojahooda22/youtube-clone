0236986e1e673333bfa6761c047526101b73d133
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-06 23:05:58 +0530

    updated

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import {Stack, Box}from '@mui/material';
-import {VideoCard, ChannelCard} from '../components';
-const videos = ({videos}) => {
+import {VideoCard, ChannelCard, Loader} from './';
 
-  console.log(videos);
+
+
+const videos = ({videos, direction}) => {
+  if (!videos?.length) return <Loader />;
   return (
     <Stack
-      direction="row" flexWrap="wrap" justifyContent="start" gap={2}
+      direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}
     >{videos.map((item, idx) =>  (
       <Box key={idx}>
         {item.id.videoId && <VideoCard video={item} />}
-        {item.id.channelId && <ChannelCard channel={item} />}
+        {item.id.channelId && <ChannelCard channelDetail={item} />}
 
       </Box>
     ))}
