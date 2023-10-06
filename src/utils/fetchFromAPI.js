399149a03de2c78e745d00383a83afc659a88c5c
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-06 23:06:10 +0530

    API updated

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,52 +1,48 @@
-// import axios from 'axios';
-
-// axios = require('axios');
-
-
-// const BASE_URL = 'https://youtube-v31.p.rapidapi.com/captions?part=snippet&videoId=M7FIvfx5J10';
-
-// let options = {
-//   method: 'get',
-//   maxBodyLength: Infinity,
-//   url: BASE_URL,
-//   headers: { 
-//     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com', 
-//     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
-//   }
-// };
-
-
-// export const fetchFromAPI = async (url) => {
-//   const {data} =  await axios.get(`${BASE_URL}/${url}`, options);
-
-//   return data;
-
-// }
-
-
 import axios from 'axios';
-axios = require('axios');
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com/search'
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-  method: 'GET',
-  url: BASE_URL,
-  params: {
-    q: 'music',
-    part: 'snippet,id',
-    regionCode: 'US',
-    maxResults: '50',
-    order: 'date'
-  },  
-  headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    params: {
+      maxResults: '50'
+    },
+  headers: { 
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com', 
+    'X-RapidAPI-Key': '598980ad1dmsh2bb2f0c22633af6p11e26cjsn935338574650'
   }
 };
+
+
 export const fetchFromAPI = async (url) => {
-    const {data} =  await axios.get(`${BASE_URL}/${url}`, options);
+  const {data} =  await axios.get(`${BASE_URL}/${url}`, options);
+  // const {data} =  await axios.get(`https://youtube-v31.p.rapidapi.com/search?q=football&part=snippet%2Cid&regionCode=IN`, options);
+  return data;
+
+}
+
+
+// import axios from 'axios';
+// axios = require('axios');
+
+// const BASE_URL = 'https://youtube-v31.p.rapidapi.com/search'
+
+// const options = {
+//   method: 'GET',
+//   url: BASE_URL,
+//   params: {
+//     q: 'music',
+//     part: 'snippet,id',
+//     regionCode: 'US',
+//     maxResults: '50',
+//     order: 'date'
+//   },  
+//   headers: {
+//     'X-RapidAPI-Key': '598980ad1dmsh2bb2f0c22633af6p11e26cjsn935338574650',
+//     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+//   }
+// };
+// export const fetchFromAPI = async (url) => {
+//     const {data} =  await axios.get(BASE_URL, options);
   
-    return data;
+//     return data;
   
-  }
