112f11f033c09a73523150b73f5c918fb34fa9a3
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-07 14:54:24 +0530

    updated search term

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
-import {Stack, Box, Typography} from '@mui/material';
+import { Box, Typography} from '@mui/material';
 import { Videos} from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { useParams } from 'react-router-dom';
+
 
 function SearchFeed () {
   const[videos, setVideos] = useState([]);
+  const {searchTerm} = useParams();
 
 
 
   useEffect(() => {
     // fetchFromAPI(`search?part=snippet&q=${selectedCategory}`) 
-    fetchFromAPI(`search?part=snippet&q=`) 
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`) 
     .then((data) => setVideos(data.items))
 
-  }, []);
+  }, [searchTerm]);
 
 
   return (
       <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
         <Typography variant='h4' fontWeight='bold' mb={2} sx={{color: 'white'}} >
           Search Results for:
-          <span style={{color: '#f31503'}}> Videos
+          <span style={{color: '#f31503'}}> {searchTerm}
 
           </span>
         </Typography>
