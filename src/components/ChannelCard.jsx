b1044404a64cb674e363f0888dc3cea7c284be33
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-07 14:37:46 +0530

    updated

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -6,7 +6,7 @@ import { demoProfilePicture } from '../utils/constants';
 
 
 
-const ChannelCard = ({channelDetail}) => (
+const ChannelCard = ({channelDetail, marginTop}) => (
 
     <Box
       sx={{
@@ -17,7 +17,8 @@ const ChannelCard = ({channelDetail}) => (
         alignItems: 'center',
         width: {xs: '356px', md: '320px' },
         height: '326px',
-        margin: 'auto'
+        margin: 'auto',
+        marginTop,
       }}
     >
       <Link
