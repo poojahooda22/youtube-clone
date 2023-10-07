8f7fa667e9899dd98f7105d37a72f0c0cb39a8b0
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-07 14:38:01 +0530

    updated

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from 'react';
+import {useState, useEffect} from 'react';
+import {Stack, Box, Typography} from '@mui/material';
+import { Videos} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
-function SearchFeed() {
+function SearchFeed () {
+  const[videos, setVideos] = useState([]);
+
+
+
+  useEffect(() => {
+    // fetchFromAPI(`search?part=snippet&q=${selectedCategory}`) 
+    fetchFromAPI(`search?part=snippet&q=${}`) 
+    .then((data) => setVideos(data.items))
+
+  }, []);
+
+
   return (
-    <div>
-      <h1>SearchFeed</h1>
-    </div>
+      <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{color: 'white'}} >
+          {}
+          <span style={{color: '#f31503'}}> Videos
+
+          </span>
+        </Typography>
+
+        <Videos videos={videos}/>
+      </Box> 
   )
 }
 
